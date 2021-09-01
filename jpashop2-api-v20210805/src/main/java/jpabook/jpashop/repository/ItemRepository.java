0146878bb49df248c48bc99210735b4bc96671ea
DUDUD11 <kim422hs@naver.com>
2021-09-01 17:05:54 +0900

    save ejecting

--- jpashop2-api-v20210805/src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ jpashop2-api-v20210805/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -2,31 +2,19 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.item.Item;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
-public class ItemRepository {
 
-    private final EntityManager em;
+public interface ItemRepository extends JpaRepository<Item,Long> {
 
-    public void save(Item item) {
-        if (item.getId() == null) {
-            em.persist(item);
-        } else {
-            em.merge(item);
-        }
-    }
+    @Query("select m from Item m where m.id=:id")
+    public Item findOne(@Param("id") Long id);
 
-    public Item findOne(Long id) {
-        return em.find(Item.class, id);
-    }
-
-    public List<Item> findAll() {
-        return em.createQuery("select i from Item i", Item.class)
-                .getResultList();
-    }
 }
